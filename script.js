62b9edbfd81eb9acdcde0cf8dca7304e6d9a0120
thais <119820325+t-lira@users.noreply.github.com>
2023-09-27 23:01:37 -0300

    btn

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-
+//
 document.addEventListener("DOMContentLoaded", function() {
   // Selecionar o formulário pelo ID
   const formulario = document.getElementById("form_register");
-
   // Adicionar um evento de envio ao formulário
   formulario.addEventListener("submit", function(event) {
       event.preventDefault(); // Impede o envio padrão do formulário
-
       // Exibir um alerta com uma mensagem
       alert("Cadastro realizado com sucesso!");
   });
 });
-
-
-
 
 
 
